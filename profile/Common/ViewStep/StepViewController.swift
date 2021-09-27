32e78ad6c5ea9004c080796958ef82926e208ee7
Nattachai nuplak <nattachai.nuplak@itone.co.th>
2021-09-27 17:28:59 +0700

    Add view stack

---
+++ profile/Common/ViewStep/StepViewController.swift
@@ -0,0 +1,17 @@
+//
+//  StepViewController.swift
+//  profile
+//
+//  Created by MosMosMos on 27/9/2564 BE.
+//
+
+import UIKit
+
+class StepViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
